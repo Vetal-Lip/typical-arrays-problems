3f6657d3d046d4f956c06ed4c857a8edd595d2c1
Vitali <terrmit574@gmail.com>
2020-03-18 21:54:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+  if (array && array.length > 0) {
+    return Math.min.apply(null, array)
+  } else return 0
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+  if (array && array.length > 0) {
+    return Math.max.apply(null, array)
+  } else return 0
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let avg = 0;
+  if (array && array.length > 0) {
+      let i = 0;
+      let counts = 0;
+      for ( i ; i < array.length; i++) {
+          avg += array[i];
+          counts++;
+      }
+      avg = avg / counts;
+  }
+
+  return avg;
+
 }
